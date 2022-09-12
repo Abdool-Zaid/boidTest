771439b66ad8090b4d5074a4c204a772a8b1cd7f
unknown <aztoorabally7447@gmail.com>
2022-09-12 12:22:37 +0200

    'update'

--- initial.js
+++ initial.js
@@ -1,29 +1,34 @@
 // console.log(document.querySelectorAll('.agent')[3].classList.value)
-let agentCount=(Math.floor( (document.documentElement.clientWidth*document.documentElement.clientHeight)/8))
+let agentCount = Math.floor(
+  (document.documentElement.clientWidth *
+    document.documentElement.clientHeight) /
+    8
+);
 // http://127.0.0.1:5500/
-console.log(agentCount)
+console.log(agentCount);
 function generateRandomColor() {
-  let letters = '0123456789ABCDEF';
-  let color = '#';
+  let letters = "0123456789ABCDEF";
+  let color = "#";
   for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
 }
-let u
-let i
-let ProxyArray=document.querySelectorAll('.agent')
-let agentArray=[]
-ProxyArray[0].style=`background-color:${generateRandomColor()}`
-agentArray.push(ProxyArray[0])
-let defaultAgent= agentArray[0]
-for(u=0;u<999  ;u++){
+let u;
+let i;
+let ProxyArray = document.querySelectorAll(".agent");
+let agentArray = [];
+ProxyArray[0].style = `background-color:${generateRandomColor()}`;
+document.querySelector("#target").innerHTML = ``;
+agentArray.push(ProxyArray[0]);
+let defaultAgent = agentArray[0];
+for (u = 0; u < 999; u++) {
   agentArray.push(defaultAgent);
-  document.querySelector('#target').innerHTML+=agentArray[u].outerHTML
-  agentArray[u].style=`background-color:${generateRandomColor()}`
+  document.querySelector("#target").innerHTML += agentArray[u].outerHTML;
+  agentArray[u].style = `background-color:${generateRandomColor()}`;
+  agentArray[u].id = "agentNO" + (u + 1);
 }
-console.log(agentArray[1].outerHTML)
-console.log(ProxyArray[0].outerHTML)
-// console.log(agentArray[0].outerHTML)
-// console.log(agentArray[7].outerHTML)
-// console.log(agentArray[10].outerHTML)
+initiateBoid = (id) => {
+  document.querySelector("#" + id).classList.toggle("active");
+  console.log(document.querySelector("#" + id).classList);
+};
